139d285ea50df076a59b9fb387db561ddabad2a1
shajanjp <shajanjp@gmail.com>
2020-12-12 10:50:48 +0530

    fix: adds user data to log

---
+++ index.js
@@ -0,0 +1,8 @@
+#!/usr/bin/env node
+console.log({
+  name: "Shajan Jacob",
+  email: "shajanjp@gmail.com",
+  website: "https://shajanjacob.com",
+  github: "shajanjp",
+  twitter: "shajanjacob",
+});
