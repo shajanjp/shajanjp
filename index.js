f9ae8a7d7aa8f3cfbca65af041ac08b3a17ec339
shajanjp <shajanjp@gmail.com>
2021-10-10 19:41:08 +0530

    feat: adds realtime data fetch

--- index.js
+++ index.js
@@ -1,8 +1,38 @@
 #!/usr/bin/env node
-console.log({
-  name: "Shajan Jacob",
-  email: "shajanjp@gmail.com",
-  website: "https://shajanjacob.com",
-  github: "shajanjp",
-  twitter: "@shajanjacob"
+const https = require('https');
+const aboutMeFilePath = "https://raw.githubusercontent.com/shajanjp/shajanjp/master/shajanjp.json"
+
+function getLatestAboutMe(filePath){
+  let aboutMe = {
+    "name": "Shajan Jacob",
+    "email": "shajanjp@gmail.com",
+    "website": "https://shajanjacob.com",
+    "job": "Senior Software Engineer",
+    "github": "https://github.com/shajanjp",
+    "twitter": "https://twitter.com/shajanjacob",
+    "linkedin": "https://www.linkedin.com/in/shajanjp"
+  };
+  
+  return new Promise((resolve, reject) => {
+    https.get(filePath, function(res){
+      let body = '';
+      
+      res.on('data', function(chunk){
+        body += chunk;
+      });
+      
+      res.on('end', function(){
+        aboutMe = JSON.parse(body);
+        
+        resolve(aboutMe);
+      });
+    }).on('error', function(e){
+      resolve(aboutMe);
+    });
+  });
+}
+
+getLatestAboutMe(aboutMeFilePath)
+.then(aboutMe => {
+  console.log(aboutMe);
 });
