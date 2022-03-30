0016dc5df96b472ee5ff1bf84ec65212a459aad7
shajanjp <shajanjp@gmail.com>
2022-03-30 21:28:01 +0530

    feat: adds typing effect to console

--- index.js
+++ index.js
@@ -2,12 +2,60 @@
 const https = require('https');
 const aboutMeFilePath = "https://raw.githubusercontent.com/shajanjp/shajanjp/master/shajanjp.json"
 
+function inColor(color = "green", text) {
+  const colorMap = {
+    green: "\x1b[32m",
+    yellow: "\x1b[33m",
+    cyan: "\x1b[36m",
+  };
+  const reset = "\x1b[0m";
+  
+  return `${colorMap[color]}${text}${reset}`;
+}
+
+function keyValueInColor(key, value) {
+  const keyValueCharStack = [];
+  
+  for (const char of key.split("")) {
+    keyValueCharStack.push(inColor("green", char));
+  }
+  
+  keyValueCharStack.push(`: "`);
+  
+  for (const char of value.split("")) {
+    keyValueCharStack.push(inColor("yellow", char));
+  }
+  
+  keyValueCharStack.push(`"`);
+  
+  return keyValueCharStack;
+}
+
+function typeTHis(data) {
+  const charStack = [];
+  
+  charStack.push(inColor("green", "{\n"));
+  
+  for (const [key, value] of Object.entries(data)) {
+    charStack.push("  ");
+    charStack.push(...keyValueInColor(key, value));
+    charStack.push("\n");
+  }
+  
+  charStack.push(inColor("green", "}\n"));
+  
+  let index = 0;
+  
+  for (const char of charStack) {
+    setTimeout(() => process.stdout.write(char), (index += 40));
+  }
+}
+
 function getLatestAboutMe(filePath){
   let aboutMe = {
     "name": "Shajan Jacob",
-    "email": "shajanjp@gmail.com",
     "website": "https://shajanjacob.com",
-    "job": "Senior Software Engineer",
+    "job": "Software Engineer",
     "github": "https://github.com/shajanjp",
     "twitter": "https://twitter.com/shajanjacob",
     "linkedin": "https://www.linkedin.com/in/shajanjp"
@@ -34,5 +82,5 @@ function getLatestAboutMe(filePath){
 
 getLatestAboutMe(aboutMeFilePath)
 .then(aboutMe => {
-  console.log(aboutMe);
+  typeTHis(aboutMe);
 });
